0679da5c68c5900c05d17d3cb0e017f9195a316b
Nishant Shrivastava <nishant_shrivastava@Nishants-Mac-mini.local>
2021-07-05 22:51:14 -0400

    Sol 2: Add two numbers rep as reversed linked list

--- leetcode-solutions.js
+++ leetcode-solutions.js
@@ -11,3 +11,29 @@ var twoSum = function (nums, target) {
     }
     return -1;
 };
+
+// 2. Add 2 numbers
+var addTwoNumbers = function (l1, l2) {
+    let p1 = l1, p2 = l2;
+    let headPtr = new ListNode();
+    let head = headPtr;
+    let carry = 0;
+    while (p1 || p2) {
+        let val1 = !p1 ? 0 : p1.val;
+        let val2 = !p2 ? 0 : p2.val;
+        let sum = val1 + val2 + carry;
+        if (sum > 9) {
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+        headPtr.next = new ListNode(sum % 10);
+        headPtr = headPtr.next;
+        p1 = !p1 ? null : p1.next;
+        p2 = !p2 ? null : p2.next;
+    }
+    if (carry > 0) {
+        headPtr.next = new ListNode(carry);
+    }
+    return head.next;
+};
