59e506c39c1282db3465d670d2857cd081acdf59
Nishant Shrivastava <nishant_shrivastava@Nishants-Mac-mini.local>
2021-11-28 16:39:44 -0500

    Solutions 11-28-2021

--- leetcode-solutions.js
+++ leetcode-solutions.js
@@ -125,6 +125,267 @@ var subsets = function (nums) {
     return ret;
 };
 
-// 4. Median of 2 sorted arrays
+// 716. Max Stack
+var MaxStack = function () {
+    this.stack = [];
+    this.maxVal = -Infinity;
+    this.maxInd = -1;
+};
+MaxStack.prototype.push = function (val) {
+    this.stack.push(val);
+    if (val >= this.maxVal) {
+        this.maxVal = val;
+        this.maxInd = this.stack.length - 1;
+    }
+};
+MaxStack.prototype.pop = function () {
+    // Is max val on top?
+    let isMaxOnTop = this.maxInd === this.stack.length - 1;
+    return isMaxOnTop ? this.popMax() : this.stack.pop();
+};
+MaxStack.prototype.top = function () {
+    if (this.stack.length === 0) return null;
+    return this.stack[this.stack.length - 1];
+};
+MaxStack.prototype.peekMax = function () {
+    return this.maxVal;
+};
+MaxStack.prototype.popMax = function () {
+    var ret = this.maxVal;
+    this.stack.splice(this.maxInd, 1);
+    // Update new Max
+    /*this.maxVal = Math.max(...this.stack);
+    this.maxInd = this.stack.indexOf(this.maxVal);*/
+    this.maxVal = -Infinity;
+    this.maxInd = -1;
+  
+    for (var i = this.stack.length - 1; i >= 0; i--) {
+        if (this.maxVal < this.stack[i]) {
+        this.maxVal = this.stack[i];
+        this.maxInd = i;
+        }
+    }  
+    // return poped max
+    return ret;
+};
 
-// 5. Longest palindromic substring
+// 244. Shortest Word Distance II
+var WordDistance = function (wordsDict) {
+    this.wordMap = {};
+    for (let i = 0; i < wordsDict.length; i++) {
+        if (this.wordMap.hasOwnProperty(wordsDict[i])) {
+            this.wordMap[wordsDict[i]].push(i);
+        } else {
+            this.wordMap[wordsDict[i]] = [i];
+        }
+    }
+};
+WordDistance.prototype.shortest = function (word1, word2) {
+    let indices1 = this.wordMap[word1], indices2 = this.wordMap[word2];
+    let minDist = Infinity;
+    for (let ind1 of indices1) {
+        for (let ind2 of indices2) {
+            minDist = Math.min(minDist, Math.abs(ind1 - ind2));
+        }
+    }
+    return minDist;
+};
+// Test Case:
+var wd = new WordDistance(["practice", "makes", "perfect", "coding", "makes"]);
+
+// 364. Nested List Weight Sum ||
+var depthSumInverse = function (nestedList) {
+    var depth = [];
+    var getDepthDFS = function (el, depth, level) {
+        depth[level] = depth[level] || [];
+        if (el.getInteger()) {
+            // Element is an integer
+            depth[level].push(el.getInteger())
+        } else {
+            // Element is an array (list)
+            for (let i of el.getList()) {
+                getDepthDFS(i, depth, level + 1);
+            }
+        }
+    };
+    for (let el of nestedList) {
+        getDepthDFS(el, depth, 0);
+    }
+    let sum = 0, len = depth.length;
+    for (let d of depth) {
+        sum = sum + d.reduce((acc, val) => {
+            return acc + (val * len);
+        }, 0);
+        len--;
+    }
+    return sum;
+};
+
+// 1086. High Five
+var highFive = function (items) {
+    var map = {};
+    for (let [id, score] of items) {
+        if (!map.hasOwnProperty(id)) {
+            map[id] = [score];
+        } else {
+            // Here multiple sorts are technicaly constant time because they are only 5 elements at max
+            let currScores = map[id];
+            if (currScores.length < 5) {
+                currScores.push(score);
+                currScores.sort((a, b) => a - b);
+            } else {
+                if (currScores[0] < score) {
+                    currScores[0] = score;
+                    currScores.sort((a, b) => a - b);
+                }
+            }
+            map[id] = currScores;
+        }
+    }
+    var ret = [];
+    for (let id in map) {
+        let currScores = map[id];
+        let currSum = currScores.reduce((ac, val) => ac + val, 0);
+        ret.push([id, Math.floor(currSum / 5)]);
+    }
+    return ret;
+};
+
+// 545. Boundary of a binary tree (Use DFS)
+var boundaryOfBinaryTree = function (root) {
+    var ret = [];
+    if (!root) return ret;
+    ret.push(root.val);
+    var dfsHelper = function (node, hasLeft, hasRight) {
+        if (!node) return;
+        if (!node.left && !node.right) {
+            ret.push(node.val);
+            return;
+        }
+        if (hasLeft) {
+            ret.push(node.val);
+        }
+        // Left subtree from node
+        dfsHelper(node.left, hasLeft, hasRight && !node.right);
+        // Right subtree from node
+        dfsHelper(node.right, hasLeft && !node.left, hasRight);
+        if (hasRight) {
+            ret.push(node.val);
+        }
+    };
+    // Left subtree from root
+    dfsHelper(root.left, true, false);
+    // Right subtree from root
+    dfsHelper(root.right, false, true);
+    return ret;
+};
+
+// 366. Find Leaves of Binary Tree
+var findLeaves = function (root) {
+    var ret = [];
+    if (!root) return ret;
+    var dfsHelper = function (node, res) {
+        if (!node) return null;
+        if (!node.left && !node.right) {
+            res.push(node.val);
+            return null;
+        }
+        node.left = dfsHelper(node.left, res);
+        node.right = dfsHelper(node.right, res);
+        return node;
+    };
+    while (root) {
+        const leafs = [];
+        root = dfsHelper(root, leafs);
+        ret.push(leafs);
+        if (!root) break;
+    }
+    return ret;
+};
+
+// 616. Add Bold Tag in String
+var addBoldTag = function (s, words) {
+    var boldIndices = new Array(s.length).fill(false);
+    for (let word of words) {
+        let start = s.indexOf(word);
+        while (start > -1) {
+            let end = start + word.length;
+            for (let i = start; i < end; i++) {
+                if (boldIndices[i]) continue;
+                boldIndices[i] = true;
+            }
+            start = s.indexOf(word, start + 1);
+        }
+    }
+    let ret = "";
+    for (let i = 0; i < s.length; i++) {
+        if (boldIndices[i] && (i === 0 || !boldIndices[i - 1])) {
+            // Start bold tag if boldIndex is true and previous bolsIndex was false
+            ret = ret + '<b>';
+        }
+        ret = ret + s[i];
+        if (boldIndices[i] && (i === s.length || !boldIndices[i + 1])) {
+            // End bold tag if string is finished
+            ret = ret + '</b>';
+        }
+    }
+    return ret;
+};
+
+// 340. Longest Substring with At Most K Distince characters
+var lengthOfLongestSubstringKDistinct = function (s, k) {
+    var len = s.length;
+    var map = new Map();
+    let l = 0, maxLen = 0;
+    for (let i = 0; i < len; i ++) {
+        let c1 = s.charAt(i);
+        if (!map.has(c1)) map.set(c1, 0);
+        map.set(c1, map.get(c1) + 1);
+        while (map.size > k) {
+            let c2 = s.charAt(l);
+            map.set(c2, map.get(c2) - 1);
+            if (map.get(c2) === 0) map.delete(c2);
+            l++;
+        }
+        maxLen = Math.max(maxLen, i - l + 1);
+    }
+    return maxLen;
+};
+
+// 259. 3Sum Smaller
+var threeSumSmaller = function (arr, target) {
+    let res = 0, len = arr.length;
+    arr = arr.sort((a, b) => a - b);
+    for (let i = 0; i < len; i++) {
+        let l = i + 1, r = len - 1;
+        while (l < r) {
+            let currSum = arr[i] + arr[l] + arr[r];
+            if (currSum < target) {
+                /* 
+                Because the array is sorted, once we find a combination that is less than our target, 
+                ALL numbers between left and right ARE ALSO combinations less than our target. 
+                Instead of iterating through them just subtract left from right to get the correct count 
+                */
+                res = res + (r - l);
+                l++;
+            } else {
+                r--;
+            }
+        }
+    }
+    return res;
+};
+
+// 362. Design Hit Counter
+var HitCounter = function () {
+    this.hits = [];
+};
+HitCounter.prototype.hit = function (timestamp) {
+    this.hits.push(timestamp);
+};
+HitCounter.prototype.getHits = function (timestamp) {
+    while (this.hits.length > 0 && timestamp - this.hits[0] >= 300) {
+        this.hits.shift();
+    }
+    return this.hits.length;
+};
